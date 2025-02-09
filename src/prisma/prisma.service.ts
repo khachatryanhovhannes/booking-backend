cabfe8324e84cb30e163692edb08b05d8de4c106
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-02-09 18:01:59 +0400

    Adding config module in prisma module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:Hov20.02@localhost:5433/',
+          url: configService.get('DATABASE_URL'),
         },
       },
     });
