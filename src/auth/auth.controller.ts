ba29f14106c43af55a87a49c15e7097c7e801861
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-02-09 21:57:33 +0400

    Adding simple auth module functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto, SignUpDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() dto: SignUpDto) {
+    return await this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  async signin(@Body() dto: SignInDto) {
+    console.log(dto);
+    return await this.authService.signin(dto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignInDto, SignUpDto } from './dto';
+import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwt: JwtService,
+    private configService: ConfigService,
+  ) {}
+  async signup(dto: SignUpDto) {
+    try {
+      const existingUser = await this.prismaService.user.findUnique({
+        where: { email: dto.email },
+      });
+
+      if (existingUser) {
+        return new ForbiddenException(`User ${dto.email} already exists`);
+      }
+
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prismaService.user.create({
+        data: {
+          ...dto,
+          password: hash,
+        },
+      });
+
+      return user;
+    } catch (err) {
+      return new InternalServerErrorException(err);
+    }
+  }
+
+  async signin(dto: SignInDto) {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { email: dto.email },
+      });
+
+      if (!user || !(await argon.verify(user.password, dto.password))) {
+        return new ForbiddenException('Invalid credentials');
+      }
+
+      const access_token = await this.signToken(user.id, user.email);
+      return { access_token };
+    } catch (err) {
+      return new InternalServerErrorException(err);
+    }
+  }
+
+  async signToken(id: number, email: string): Promise<string> {
+    const payload = { sub: id, email };
+
+    return this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: this.configService.get<string>('JWT_SECRET'),
+    });
+  }
+}
